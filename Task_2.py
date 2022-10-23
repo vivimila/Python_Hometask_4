d59acf027ccf9cca98fba12926753a88798659f0
vivimila <violamilashevskaya@yandex.by>
2022-10-23 19:54:59 +0300

    work with folder 2

--- Task_2.py
+++ Task_2.py
@@ -1,2 +1,12 @@
 #Задайте последовательность чисел. 
 #Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+#specifiedList = list(map(int, input("Введите числа через пробел:\n").split()))
+specifiedList = 1, 2, 5, 45, 7, 9, 45, 5, 3
+#print(f"Исходный список: {specifiedList}")
+
+new_list = []
+new_list.append(i):
+    for i in specifiedList 
+        if i not in new_list
+print(f"Из списка {specifiedList}\nнеповторяющиеся элементы: {new_list}")
