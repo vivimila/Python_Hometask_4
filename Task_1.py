03eca5a15524e873e8419aa107099e55b280638d
vivimila <violamilashevskaya@yandex.by>
2022-10-23 19:40:14 +0300

    task 1 is solved

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,16 @@
 #Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+num = int(input("Введите число: "))
+
+i = 2 
+listResult = []
+
+while i <= num:
+    if num % i == 0:
+        listResult.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
+
+print(f"Множители заданного числа: {listResult}")
